1015107882f245db898e8714ca856f4b4ed19168
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2022-09-17 01:52:22 +0200

    Add Css to the Main component - not ready

--- portfolio/src/components/Main.jsx
+++ portfolio/src/components/Main.jsx
@@ -1,42 +1,47 @@
 import React from "react";
+import logo from "../assets/GS-logo.svg";
+import "./Main.css";
+import { NavLink } from "react-router-dom";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 function Main() {
   return (
     <>
       <div className="main-container">
         <div className="main-blue-light"></div>
-        <nav className="navigation-bar">
+        <header>
           <div className="logo">
-            <img src="" alt="" />
+            <img src={logo} alt="logo" />
             <div className="dash"></div>
           </div>
 
-          <div className="nav-items">
-            <a className="nav-links" href="#">
+          <nav className="nav-bar">
+            <a className="nav-link" href="#">
               home
             </a>
-            <a className="nav-links" href="#">
+            <a className="nav-link" href="#">
               portfolio
             </a>
-            <a className="nav-links" href="#">
+            <a className="nav-link" href="#">
               about
             </a>
-            <a className="nav-links" href="#">
+            <a className="nav-link" href="#">
               skills
             </a>
-            <a className="nav-links" href="#">
+            <a className="nav-link" href="#">
               contact
             </a>
-          </div>
-        </nav>
+          </nav>
+        </header>
 
         <div className="main-title">
           <h1 className="front-title">Gábor Szabó</h1>
-          <h1 className="back-title">Gábor Szabó</h1>
+          <h1 className="back-title">GÁBOR SZABÓ</h1>
         </div>
-
-        <p className="time">11:23</p>
-        <i className="fa-solid fa-angle-down"></i>
+        <div className="bottom-section">
+          <p className="time">11:23</p>
+          <i className="fa-solid fa-angle-down"></i>
+        </div>
       </div>
     </>
   );
